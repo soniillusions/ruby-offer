da2e333f367dc6ae83fa502bbd803ef03a9af69b
Aleksey <alekseycrosso@gmail.com>
2024-03-23 17:24:56 +0300

    add resume form and validation

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,2 +1,6 @@
 class Resume < ApplicationRecord
+  validates :experience, presence: true, numericality: { greater_than: 0, less_than: 40 }
+  validates :resume_link, format: { with: URI::DEFAULT_PARSER.make_regexp }, presence: true
+  validates :github_link, format: { with: URI::DEFAULT_PARSER.make_regexp }, presence: true
+  validates :body, presence: true
 end
