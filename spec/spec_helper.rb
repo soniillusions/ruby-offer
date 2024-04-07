56f577092a08ce5b99013b38a1c4ff549062aad1
Aleksey <alekseycrosso@gmail.com>
2024-04-07 20:09:16 +0300

    create session_helper.rb with sign_up method, add visitor_creates_account_spec.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'capybara/rspec'
+require 'support/session_helper'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
---
+++ spec/support/session_helper.rb
@@ -0,0 +1,10 @@
+def sign_up
+  visit new_user_registration_path
+
+  fill_in :user_email, :with => 'user@example.com'
+  fill_in :user_username, :with => 'Grisha'
+  fill_in :user_password, :with => '123321'
+  fill_in :user_password_confirmation, :with => '123321'
+
+  click_button 'Sign up'
+end
