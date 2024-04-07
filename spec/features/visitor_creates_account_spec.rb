3725914424b01f9f2671d0b25f08094b9c7d24b5
Aleksey <alekseycrosso@gmail.com>
2024-04-07 21:10:22 +0300

    update visitor_creates_account_spec.rb

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -1,8 +1,13 @@
 require "spec_helper"
 
 feature "Create Account" do
-  scenario "allows visitor to create account" do
+  before(:each) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
     sign_up
+  end
+
+  scenario "allows visitor to create account" do
     expect(page).to have_content I18n.t "devise.registrations.signed_up"
   end
 end
